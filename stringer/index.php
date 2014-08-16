cf32493c5671feca4203d0795f2351fb075c2abe
Kelli Muhindi <muhindi09@gmail.com>
2014-08-16 09:45:24 +0300

    Added directory stringer and its index page

---
+++ stringer/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <link rel="stylesheet" href="../css/styler.css" type="text/css"/>
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "This is the default Devlib/Stringer Page";      
+        ?>
+    </body>
+</html>
